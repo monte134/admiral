5d953effef4bcb09e762eb2447e58c67d1eeedd9
monte134 <monte134@ukr.net>
2019-05-22 18:04:39 +0300

    v1

---
+++ getnews_unn.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+curl https://www.unn.com.ua --output buf1
+cat buf1 | grep "news_public_*******" >tmp1
+cat tmp1 | grep ":" >tmp2
+
+cat tmp2 | sed -e 's/<[^>]*>//g' >tmp1
+cat tmp1 | sed 's/^[ t]*//' >tmp2
+cat tmp2 | sort >res
+
+#clean
+
+rm buf1
+rm tmp1
+rm tmp2
+
+#cat res
+clear
+cat res
+
+
+
+
+
+
+
